1eb706ffd1490d86c8eeb73195c5b32f929d601c
Rajesh Roy <rajeshroy402@gmail.com>
2023-08-26 13:10:52 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,9 @@ echo "copy service files"
 sudo cp restart* /etc/systemd/system/
 
 cd /etc/systemd/system/
+sudo systemctl start restart_sheet.service
 sudo systemctl enable restart_sheet.service
+sudo systemctl start restart_speed.service
 sudo systemctl enable restart_speed.service
 
 cd /home/nvidia/inv
