3fe04898212178b90af329192365bc5da9026a7a
Rajesh Roy <rajeshroy402@gmail.com>
2023-08-26 12:51:39 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,12 @@
 sudo apt install speedtest-cli
+
+echo "copy service files"
+
+sudo cp restart* /etc/systemd/system/
+
+cd /etc/systemd/system/
+sudo systemctl enable restart_sheet.service
+sudo systemctl enable restart_speed.service
+
+cd /home/nvidia/inv
+
